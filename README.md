db0be0d1b585c63ac2378a4d0d852678b5574c35
Marfullsen <marfullsen@gmail.com>
2021-04-24 19:34:06 -0400

    Spanish translation added

---
+++ README.md
@@ -0,0 +1,18 @@
+# AoE2 recgame info
+Get some important info about the recorded games from Age of Empires 2.
+
+[![Python](https://img.shields.io/badge/Python-3.7.7-blue.svg)](https://www.python.org/)
+[![Mgz](https://img.shields.io/badge/Mgz-1.5.0-green.svg)](https://pypi.org/project/mgz/)
+
+[![screenshot](./screenshots/saved-info.svg)](https://github.com/Marfullsen/AoE2-saved-recgame-info)
+
+## Description
+Get important info about the recorded games like map, the time played, the teams, and many more.
+
+## Reason of the project
+This project will be part of the Warriors Clan webpage, and will be merged with [AoE Minimap Generator](https://github.com/Marfullsen/AoE2-minimap-generator) to get something amazing like [RecGames.es](https://github.com/goto-bus-stop/recgames).
+
+## Resources.
+[Mgz](https://pypi.org/project/mgz/)
+[RecAnalyst](https://github.com/goto-bus-stop/recanalyst)
+[RecGames](https://github.com/goto-bus-stop/recgames)
--- main.py
+++ main.py
@@ -2,51 +2,79 @@
 #-*- coding: utf-8 -*-
 
 import glob
+
 import mgz
 from mgz.summary import Summary
 
-input_file = glob.glob('./*.mgx')[0]
+from en_to_es import en_es
 
-to_spanish = {'fast':'rádida', 'normal':'normal'}
-
-with open(f'{input_file}', 'rb') as data:
+def get_info(input_file:str) -> dict():
+    '''Obtiene la información más relevante de la partida'''
+    info = dict()
+    with open(f'{input_file}', 'rb') as data:
         sumario = Summary(data)
-        duracion_partida = mgz.util.convert_to_timestamp(sumario.get_duration()/1000)
-        punto_de_vista = sumario.get_players()[sumario.get_owner()-1]['name']
-        mapa_revelado = sumario.get_settings()['map_reveal_choice'][1]
-        velocidad = to_spanish[sumario.get_settings()['speed'][1]]
-        poblacion = sumario.get_settings()['population_limit']
-        diplomacia = sumario.get_diplomacy()['type']
-        nombre_mapa = sumario.get_map()['name']
-        tamano_mapa = sumario.get_map()['size']
-        teams = ''
-        if diplomacia == 'TG':
-            teams = sumario.get_diplomacy()['team_size']
+    info['nombre_archivo'] = input_file
+    info['duracion_partida'] = mgz.util.convert_to_timestamp(sumario.get_duration()/1000)
+    info['punto_de_vista'] = sumario.get_players()[sumario.get_owner()-1]['name']
+    info['mapa_revelado'] = en_es['reveal_map'][sumario.get_settings()['map_reveal_choice'][1].capitalize()]
+    info['velocidad'] = en_es['game_speeds'][sumario.get_settings()['speed'][1].capitalize()]
+    info['poblacion'] = sumario.get_settings()['population_limit']
+    info['diplomacia'] = sumario.get_diplomacy()['type']
+    info['nombre_mapa'] = en_es['map_names'][sumario.get_map()['name']]
+    info['tamano_mapa'] = sumario.get_map()['size'].capitalize()
+    for map_size in en_es['map_sizes'].keys():
+            if info['tamano_mapa'] in map_size:
+                    info['tamano_mapa'] = en_es['map_sizes'][map_size]
+    info['teams'] = ''
+    if info['diplomacia'] == 'TG':
+        info['teams'] = sumario.get_diplomacy()['team_size']
+        info['diplomacia'] = 'Batalla de equipos'
+        
+    equipos = dict()
+    for index, team in enumerate(sumario.get_teams()):
+        index += 1
+        equipos[index] = dict()
+        for jugador in team:
+            equipos[index][jugador] = dict()
+            equipos[index][jugador]['nickname'] = sumario.get_players()[jugador-1]['name']
+            civ = str(sumario.get_players()[jugador-1]['civilization'])
+            civ = sumario.reference['civilizations'][civ]['name']
+            equipos[index][jugador]['civ'] = en_es['civilizations'][civ]
+            if sumario.get_players()[jugador-1]['winner']:
+                equipos[index][jugador]['victoria'] = True
+            else:
+                equipos[index][jugador]['victoria'] = False
+            id_color = str(sumario.get_players()[jugador-1]['color_id'])
+            equipos[index][jugador]['color'] = mgz.reference.get_consts()['player_colors'][id_color]
+    info['equipos'] = equipos
 
-print(f"Info sobre la partida grabada {input_file}")
-print(f"Duración de la partida: {duracion_partida}")
-print(f"Punto de vista de: {punto_de_vista}")
-print()
-print(f"Mapa jugado: {nombre_mapa}")
-print(f"Tamaño mapa: {tamano_mapa}")
-print(f"Revelar mapa: {mapa_revelado}")
-print(f"Velocidad del juego: {velocidad}")
-print(f"Población máxima: {poblacion}")
-print(f"Diplimacia: {diplomacia}")
-if teams: print(f"Diplomacia de los equipos: {teams}")  
-print()
-print("Equipos")
-for team in sumario.get_teams():
-	for jugador in team:
-		print(sumario.get_players()[jugador-1]['name'])
-		civ = str(sumario.get_players()[jugador-1]['civilization'])
-		print(sumario.reference['civilizations'][civ]['name'])
-		if sumario.get_players()[jugador-1]['winner']:
-			print(f"Perdedor")
-		else:
-			print("Ganador")
-		id_color = str(sumario.get_players()[jugador-1]['color_id'])
-		#print(f'id_color: {id_color}')
-		color = mgz.reference.get_consts()['player_colors'][id_color]
-		print(color)
-	print()
+    return info
+
+if __name__== "__main__":
+    input_file = glob.glob('*.mgx')[0]
+    info = get_info(input_file)
+    print(f"Info sobre la partida grabada '{info['nombre_archivo']}'")
+    print(f"Duración de la partida: {info['duracion_partida']}")
+    print(f"Punto de vista de: {info['punto_de_vista']}")
+    print()
+    print(f"Mapa jugado: {info['nombre_mapa']}")
+    print(f"Tamaño mapa: {info['tamano_mapa']}")
+    print(f"Revelar mapa: {info['mapa_revelado']}")
+    print(f"Velocidad del juego: {info['velocidad']}")
+    print(f"Población máxima: {info['poblacion']}")
+    print(f"Diplomacia: {info['diplomacia']}")
+    if info['teams']:
+        print(f"Diplomacia de los equipos: {info['teams']}")
+    print()
+    for indice, equipo in info['equipos'].items():
+        print(f'Equipo {indice}')
+        for jugador, dato in equipo.items():
+            print(f'{jugador}) ', end='')
+            print(dato['nickname'], end=' - ')
+            print(dato['civ'], end=' - ')
+            print(dato['color'])
+            if dato['victoria']:
+                print('Equipo ganador de la partida.')
+            else:
+                print('Equipo pierde esta ronda.')
+        print()
